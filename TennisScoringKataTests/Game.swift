ab6ea6e88ef31f9042678b79d3a313d9a4d6c8e8
Daniel Reinhart <Danielr@procore.com>
2017-03-24 09:43:39 -0700

    WIP. abandoning this...Definetely a good idea to stop at commit before and refactor. Going to leave this branch hanging to see the difference.

--- TennisScoringKataTests/Game.swift
+++ TennisScoringKataTests/Game.swift
@@ -3,6 +3,9 @@ import Foundation
 //this class represents a single tennis game.
 
 class Game : NSObject {
+
+    static let kAdvantage = 100
+
     var score: String {
         get {
             if "\(servingPlayerScore) - \(receivingPlayerScore)" == "40 - 40" {
@@ -16,9 +19,14 @@ class Game : NSObject {
     fileprivate var servingPlayerScore : Int = 0
     fileprivate var receivingPlayerScore : Int = 0
 
+    //WOW this is so bad.
     func servingPlayerWinPoint() {
         if servingPlayerScore == 30 {
             servingPlayerScore += 10
+        } else if servingPlayerScore == 40 {
+            servingPlayerScore = Game.kAdvantage
+        } else if receivingPlayerScore == Game.kAdvantage {
+            receivingPlayerScore = 40
         } else {
             servingPlayerScore += 15
         }
@@ -27,6 +35,10 @@ class Game : NSObject {
     func receivingPlayerWinPoint() {
         if receivingPlayerScore == 30 {
             receivingPlayerScore += 10
+        } else if receivingPlayerScore == 40 {
+            receivingPlayerScore = Game.kAdvantage
+        } else if receivingPlayerScore == Game.kAdvantage {
+            servingPlayerScore = 40
         } else {
             receivingPlayerScore += 15
         }
